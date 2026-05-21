03fde5a21135b888412db101bf48354a3ae7f1be
Arijit Karmakar <arijitkarmakar873@gmail.com>
2026-05-21 12:37:19 +0530

    create About Me section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,11 +21,8 @@ const HeroSection = () => {
             </span>
           </h1>
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            Enthusiastic software developer with a Master's in Computer
-            Applications (MCA, 2024). Passionate about problem-solving,
-            proficient in C++. Eager to learn and work with new technologies,
-            with knowledge of the MERN stack. Aspiring to grow as a skilled
-            developer and contribute to innovative projects.
+            Competitive Programmer | Problem Solving Enthusiast | MERN Stack
+            Developer
           </p>
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
             <a href="#projects" className="cosmic-button">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "@/components/StarBackground";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
       {/* Main Content */}
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
       {/* Footer */}
     </div>
